d377a2d750ebcf7b6883a30be5de8d70aa997d27
tunadeveloper <tunabusiness25@gmail.com>
2025-09-13 20:09:05 +0300

    Updated

---
+++ 19-RestaurantTableCurrentStatusProject/Form2.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace _19_RestaurantTableCurrentStatusProject
+{
+    partial class Form2
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel panelMasalar;
+        private System.Windows.Forms.Panel panelFooter;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelMasalar = new System.Windows.Forms.Panel();
+            this.panelFooter = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(128)))));
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(613, 60);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "🍽️ Restoran Masa Durumu";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelMasalar
+            // 
+            this.panelMasalar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.panelMasalar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMasalar.Location = new System.Drawing.Point(0, 60);
+            this.panelMasalar.Name = "panelMasalar";
+            this.panelMasalar.Size = new System.Drawing.Size(613, 281);
+            this.panelMasalar.TabIndex = 0;
+            // 
+            // panelFooter
+            // 
+            this.panelFooter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(128)))));
+            this.panelFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelFooter.Location = new System.Drawing.Point(0, 341);
+            this.panelFooter.Name = "panelFooter";
+            this.panelFooter.Size = new System.Drawing.Size(613, 50);
+            this.panelFooter.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(613, 391);
+            this.Controls.Add(this.panelMasalar);
+            this.Controls.Add(this.panelFooter);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Restoran Masa Takip";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+    }
+}
---
+++ 19-RestaurantTableCurrentStatusProject/Form2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _19_RestaurantTableCurrentStatusProject
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (var context = new TableCurrentStatusDbEntities())
+            {
+                var buttons = panelMasalar.Controls.OfType<Button>().ToList();
+                foreach (var btn in buttons)
+                {
+                    panelMasalar.Controls.Remove(btn);
+                    btn.Dispose();
+                }
+
+                var values = context.Tables.ToList();
+                int buttonSize = 90;
+                int padding = 30;
+                int xOffset = 20;
+                int yOffset = 20;
+
+                ToolTip tt = new ToolTip();
+
+                for (int i = 0; i < values.Count; i++)
+                {
+                    var item = values[i];
+                    Button button = new Button();
+                    button.Text = item.TableNumber;
+                    button.Size = new Size(buttonSize, buttonSize);
+                    button.Location = new Point(
+                        xOffset + (i % 5) * (buttonSize + padding),
+                        yOffset + (i / 5) * (buttonSize + padding));
+
+                    button.Font = new Font("Segoe UI", 9, FontStyle.Bold);
+                    button.ForeColor = Color.White;
+                    button.FlatStyle = FlatStyle.Flat;
+                    button.FlatAppearance.BorderSize = 0;
+
+                    System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+                    path.AddEllipse(0, 0, button.Width, button.Height);
+                    button.Region = new Region(path);
+
+                    if (item.Status.GetValueOrDefault())
+                    {
+                        button.BackColor = Color.FromArgb(0, 180, 120);
+                        tt.SetToolTip(button, "Masa boş");
+                    }
+                    else
+                    {
+                        button.BackColor = Color.FromArgb(200, 60, 80);
+                        tt.SetToolTip(button, "Masa dolu");
+                    }
+
+                    button.MouseEnter += (s, ev) =>
+                    {
+                        button.BackColor = ControlPaint.Light(button.BackColor, 0.25f);
+                    };
+                    button.MouseLeave += (s, ev) =>
+                    {
+                        button.BackColor = item.Status.GetValueOrDefault()
+                            ? Color.FromArgb(0, 180, 120)
+                            : Color.FromArgb(200, 60, 80);
+                    };
+
+                    button.Click += (s, ev) =>
+                    {
+                        MessageBox.Show($"Seçilen {button.Text} durumu: " +
+                            (item.Status.GetValueOrDefault() ? "Boş" : "Dolu"));
+                    };
+
+                    panelMasalar.Controls.Add(button);
+                }
+            }
+        }
+
+    }
+}
